207445622b8a6486a519a3635c0e5b7001589a6a
Darren Ackers <darrenackers@Darrens-MacBook-Pro.local>
2017-10-18 14:53:12 +0100

    App and home screen added

---
+++ src/screens/Deliveries.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { AppRegistry, Text } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+
+class DeliveriesScreen extends React.Component {
+    static navigationOptions = {
+        title: 'Welcome',
+    };
+    render() {
+        return <Text>Home Page</Text>;
+    }
+}
+
+export const SimpleApp = StackNavigator({
+    Home: { screen: HomeScreen },
+});
+
+AppRegistry.registerComponent('SimpleApp', () => SimpleApp);
